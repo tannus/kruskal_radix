13a528008a9534bc369fe38f7a22ace3e71ca057
tannus <tiagomt@dcc.ufmg.br>
2019-06-05 12:15:12 -0300

    Fixing index from int to longlong to avoid overflow

--- src/test_gen.cpp
+++ src/test_gen.cpp
@@ -7,19 +7,20 @@ int main(int argc, char** argv){
     ifstream input(argv[1]);
     ofstream output(argv[2]);
     
-    int density,n_digits,vertex;
+    int density,n_digits;
+    long long int vertex;
 
     input >> density >> n_digits >> vertex; 
 
-    int n_edges = vertex*(vertex-1)/2;
+    long long int n_edges = vertex*(vertex-1)/2;
 
     srand (time(NULL));
 
-    vector<vector<int> > edges(n_edges,vector<int>(3));
+    vector<vector<long long int> > edges(n_edges,vector<long long int>(3));
 
-    int e = 0;
-    for(int i = 1; i < vertex; i++){
-        for(int j = 0; j < i; j++){
+    long long int e = 0;
+    for(long long int i = 1; i < vertex; i++){
+        for(long long int j = 0; j < i; j++){
             edges[e][0] = i;
             edges[e][1] = j;
             edges[e][2] = rand() % (int)(pow(10,n_digits));
@@ -29,10 +30,10 @@ int main(int argc, char** argv){
 
     random_shuffle(edges.begin(),edges.end());
 
-    int n = n_edges*(0.01*density);
+    long long int n = n_edges*(0.01*density);
 
     output << n << endl;
-    for(int i = 0; i < n; i++){
+    for(long long int i = 0; i < n; i++){
         output << edges[i][0] <<" "<< edges[i][1] <<" "<< setfill('0') << setw(n_digits) << edges[i][2] << endl;
     }
 
